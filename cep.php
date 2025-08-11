4ce8a1a933f59dfe1354876aefd9ccc64f5498fd
tiago219 <tiagosasak.gmail.com>
2025-08-11 17:33:29 -0300

    aa

---
+++ cep.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultar Cep</title>
+</head>
+<body>
+
+    <input name="cep" id="cep" type="text" />
+    <button onclick="">Consultar Cep</button>
+    <script>
+        var cep = document.getElementById("cep")
+    </script>
+    
+</body>
+</html>
